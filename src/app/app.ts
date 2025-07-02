df68855702b63c8bcd2e00901f5917fbb0d1647e
Sergio Barreras <sergiobg.isc@gmail.com>
2025-07-01 20:30:19 -0700

    Implements basic landing page structure
    Adds initial structure for the landing page with header, about, services, contact and footer components.
    
    Also, sets the page icon and integrates tailwindcss.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { About } from "./components/about/about";
+import { Services } from "./components/services/services";
+import { Contact } from "./components/contact/contact";
+import { Footer } from "./components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, About, Services, Contact, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected title = 'botbite-lander';
